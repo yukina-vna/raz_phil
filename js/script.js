3f813fcf024261036f8d05110add2d91a0006123
yukina-vna <140179996+yukina-vna@users.noreply.github.com>
2024-03-12 01:00:01 +0900

    修正版

--- js/script.js
+++ js/script.js
@@ -70,3 +70,16 @@ $(function () {
         $(".sp_nav").fadeToggle();
     });
 });
+
+// コンセプトのアニメーション
+$(window).scroll(function () {
+    $(".fadein").each(function () {
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+
+        if (scroll > elemPos - windowHeight + 150) {
+            $(this).addClass("scrollin");
+        }
+    });
+});
